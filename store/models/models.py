e89e0540857146465035b7f6a321b7560c8abf8b
vadlord <vadim.t0lstykh@yandex.ru>
2021-12-13 17:00:09 +0300

    add new models

--- store/models/models.py
+++ store/models/models.py
@@ -1,17 +1,33 @@
-class User:
-    def __init__(self):
-        self.name = ''
-        self.surname = ''
-        self.login = ''
-        self.hash_pass = ''
-        self.photo = None
-        self.id = -1
-        self.data = ''
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
-class Message:
-    def __init__(self):
-        self.text = ''
-        self.time_send = ''
-        self.is_read = False
-        self.is_important = False
-        self.is_edited = False
+
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    login = Column(String, unique=True, nullable=False)
+    hash_password = Column(String)
+    name = Column(String)
+    surname = Column(String)
+    photo = Column(String)
+    data = Column(String)
+    messages = relationship('Message', backref='user')
+
+
+
+class Message(Base):
+    __tablename__ = 'message'
+
+    id = Column(Integer, primary_key=True)
+    text = Column(String)
+    time_send = Column(DateTime)
+    is_read = Column(Boolean)
+    is_important = Column(Boolean)
+    is_edited = Column(Boolean)
+    author_id = Column(Integer, ForeignKey('user.id'))
+
