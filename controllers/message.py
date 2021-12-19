c51f7b5cb80bdca15fba1d20340862eb35a2a82e
Chernenkiy <nikmbogdanov@gmail.com>
2021-12-19 20:14:48 +0300

    Incorrect name of UserBase class's field fixed

--- controllers/message.py
+++ controllers/message.py
@@ -2,12 +2,13 @@ import requests
 from store.models.models import Message
 
 
-def get_message(message_id):
-    message = Message(**requests.get(f"http://127.0.0.1:8000/messages/{message_id}").json())
+def get_message(message_id: int):
+    response = requests.get(f"http://127.0.0.1:8000/messages/{message_id}")
+    message = Message(**(response.json()))
     return message
 
 
-def get_messages_for_dialogue(from_id: int, to_id: int):
-    response = requests.get(f"http://127.0.0.1:8000/messages/{from_id}/{to_id}")
+def get_messages_for_dialogue(user1_id: int, user2_id: int):
+    response = requests.get(f"http://127.0.0.1:8000/messages/{user1_id}/{user2_id}")
     messages = [Message(**message_params) for message_params in response.json()]
     return messages
