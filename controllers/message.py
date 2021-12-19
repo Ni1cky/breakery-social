1561fa94ed6b46ff66abb00a8630fc9c0b8679e1
Chernenkiy <nikmbogdanov@gmail.com>
2021-12-19 16:10:53 +0300

    Getting messages and users fixed

--- controllers/message.py
+++ controllers/message.py
@@ -1,11 +1,13 @@
 import requests
+from store.models.models import Message
 
 
 def get_message(message_id):
-    message = requests.get(f"http://127.0.0.1:8000/messages/{message_id}")
+    message = Message(**requests.get(f"http://127.0.0.1:8000/messages/{message_id}").json())
     return message
 
 
 def get_messages_for_dialogue(from_id: int, to_id: int):
-    messages = requests.get(f"http://127.0.0.1:8000/messages/{from_id}/{to_id}").json()
+    response = requests.get(f"http://127.0.0.1:8000/messages/{from_id}/{to_id}")
+    messages = [Message(**message_params) for message_params in response.json()]
     return messages
--- controllers/user.py
+++ controllers/user.py
@@ -1,11 +1,13 @@
 import requests
+from store.models.models import User
 
 
 def get_users():
-    users = requests.get("http://127.0.0.1:8000/users").json()
+    response = requests.get("http://127.0.0.1:8000/users")
+    users = [User(**user_params) for user_params in response.json()]
     return users
 
 
 def get_user(user_id):
-    user = requests.get(f"http://127.0.0.1:8000/users/{user_id}").json()
+    user = User(**requests.get(f"http://127.0.0.1:8000/users/{user_id}").json())
     return user
