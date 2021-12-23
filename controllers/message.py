fe3befad99f594651cdb5d0161ad029373211b02
vadlord <vadim.t0lstykh@yandex.ru>
2021-12-23 21:10:00 +0300

    add get_time_send_sorted_message

--- controllers/message.py
+++ controllers/message.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import requests
 from store.models.models import Message
 
@@ -12,3 +14,11 @@ def get_messages_for_dialogue(user1_id: int, user2_id: int):
     response = requests.get(f"http://127.0.0.1:8000/messages/{user1_id}/{user2_id}")
     messages = [Message(**message_params) for message_params in response.json()]
     return messages
+
+
+def get_time_send_sorted_message(sender_id: int, recipient_id: int):
+    messages = get_messages_for_dialogue(sender_id, recipient_id)
+    messages_sort = sorted(messages,
+                           key=lambda task: datetime.strptime(task.deadline, "%Y-%m-%d").date(),
+                           reverse=True)
+    return messages_sort
