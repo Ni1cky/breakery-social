c3c8fedbd8069b32c692730d85a07e2a66d2527f
Chernenkiy <nikmbogdanov@gmail.com>
2021-12-26 12:32:10 +0300

    Now nothing works before authorization

--- social.py
+++ social.py
@@ -1,10 +1,9 @@
 from kivy.core.window import Window
 from kivymd.app import MDApp
-from screen_manager import Manager
 from main_menu.menu import MainMenuScreen
-from views import meta
 
 Window.size = (400, 700)
+
 
 class SocialApp(MDApp):
 
@@ -14,4 +13,3 @@ class SocialApp(MDApp):
 
 if __name__ == '__main__':
     SocialApp().run()
-
