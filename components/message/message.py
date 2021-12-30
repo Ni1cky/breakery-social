72cb16d5074d5bb33c19a594e3ca14eedac9ab0b
Chernenkiy <nikmbogdanov@gmail.com>
2021-12-30 20:57:56 +0300

    rv changed to recycle_view

--- components/message/message.py
+++ components/message/message.py
@@ -13,22 +13,22 @@ from kivy.uix.recycleview.views import RecycleDataViewBehavior
 
 class MessageWidget(RecycleDataViewBehavior, BoxLayout):
     _latest_data = None
-    _rv = None
+    _recycle_view = None
     message_text = StringProperty()
     message = ObjectProperty()
     send_from_me = NumericProperty()
     textik = ObjectProperty()
 
-    def refresh_view_attrs(self, rv, index, data):
-        self._rv = rv
+    def refresh_view_attrs(self, recycle_view, index, data):
+        self._recycle_view = recycle_view
         if self._latest_data is not None:
             self._latest_data["height"] = self.height
         self._latest_data = data
         print(self._latest_data)
-        super(MessageWidget, self).refresh_view_attrs(rv, index, data)
+        super(MessageWidget, self).refresh_view_attrs(recycle_view, index, data)
 
     def on_height(self, instance, value):
         data = self._latest_data
         if data is not None and data["height"] != value:
             data["height"] = value
-            self._rv.refresh_from_data()
+            self._recycle_view.refresh_from_data()
