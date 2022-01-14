2128d4916032cfc4ae0a92bcdd26962052a5dc3a
vadlord <vadim.t0lstykh@yandex.ru>
2022-01-14 13:47:28 +0300

    authorization done

--- views/authorization/authorization.py
+++ views/authorization/authorization.py
@@ -1,6 +1,8 @@
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 import requests
+from requests import Request
+
 from views.base import BaseScreen
 from views import meta
 
@@ -12,17 +14,27 @@ class LoginScreen(BaseScreen):
     login = ObjectProperty()
     passw = ObjectProperty()
 
+
     def go_to_main_screen(self):
-
-        token = requests.post('http://127.0.0.1:8000/token', headers={'accept': 'application/json', 'Content-Type': 'application/x-www-form-url-encoded'}, data={ 'grant_type': '', 'username': self.login.text, 'password': self.passw.text, 'scope': '', 'client_id': '', 'client_secret': ''})
-
-        print(token.request.body, token.request.url, token.request.headers)
-        print(token.text)
-
+        r = requests.Request('GET', 'http://127.0.0.1:8000/users/me')
+        user = self.send(r)
+        print(user.text)
+        self.manager.current = meta.SCREENS.PROFILE_SCREEN
 
 
+    def send(self, req: Request):
+        token = requests.post('http://127.0.0.1:8000/token',
+                              data={'grant_type': '', 'username': self.login.text, 'password': self.passw.text,
+                                    'scope': '', 'client_id': '', 'client_secret': ''})
+        req.headers[
+            'Authorization'] = f"Bearer {token.json()['access_token']}"
+        s = requests.Session()
+        resp = s.send(req.prepare())
+        return resp
 
-        #self.manager.current = meta.SCREENS.NEWS_SCREEN
+
+
+
 
 
 class RegisterScreen(BaseScreen):
