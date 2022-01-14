9cee121758f2cb3edd8545af9e673a9ddd7d54e5
vadlord <vadim.t0lstykh@yandex.ru>
2022-01-14 09:51:47 +0300

    nothing work

--- views/authorization/authorization.py
+++ views/authorization/authorization.py
@@ -1,4 +1,6 @@
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+import requests
 from views.base import BaseScreen
 from views import meta
 
@@ -7,9 +9,13 @@ Builder.load_file('views/authorization/authorization.kv')
 
 class LoginScreen(BaseScreen):
     SCREEN_NAME = meta.SCREENS.LOGIN_SCREEN
-
+    login = ObjectProperty()
+    passw = ObjectProperty()
     def go_to_main_screen(self):
-        self.manager.current = meta.SCREENS.NEWS_SCREEN
+        #print(self.login.text, self.passw.text)
+        r = requests.get('http://127.0.0.1:8000/users/me')
+        print(r.text)
+        #self.manager.current = meta.SCREENS.NEWS_SCREEN
 
 
 class RegisterScreen(BaseScreen):
