18ec097e27a8c4112fbb7496fe0422d554dc4c92
Chernenkiy <nikmbogdanov@gmail.com>
2022-01-15 19:56:08 +0300

    After-merge conflicts solved and messages now load to message screen

--- views/dialog_screen/dialog_screen.py
+++ views/dialog_screen/dialog_screen.py
@@ -1,16 +1,10 @@
-import datetime
-import random
-
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
-from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.recycleview import RecycleView
-from kivymd.uix.list import MDList
 import current_user
 from store.models.models import Message
 from views.base import BaseScreen
 from views.meta import SCREENS
-from components.message.message import MessageWidget
 from controllers.message import get_time_send_sorted_message
 
 Builder.load_file('views/dialog_screen/dialog_screen.kv')
@@ -19,21 +13,20 @@ Builder.load_file('views/dialog_screen/dialog_screen.kv')
 class DialogScreen(BaseScreen):
     SCREEN_NAME = SCREENS.DIALOG_SCREEN
     scrollable_messages: RecycleView = ObjectProperty()
-    messages: MDList = ObjectProperty()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.scrollable_messages.data = []
 
-        for i in range(1, 40):
-            print(len(i * "привет") // 20 * 100)
-            self.scrollable_messages.data.append({'message_text': i * "привет)",
-                                                        'time_send': datetime.datetime(2019, 6, 1, 12, 22),
-                                                        'is_read': False,
-                                                        'is_important': False,
-                                                        'is_edited': False,
-                                                  "height": int(max(len(i * "привет") / 40, 1) * 50),
-                                                'send_from_me': i % 2})
+        # for i in range(1, 40):
+        #     print(len(i * "привет") // 20 * 100)
+        #     self.scrollable_messages.data.append({'message_text': i * "привет)",
+        #                                                 'time_send': datetime.datetime(2019, 6, 1, 12, 22),
+        #                                                 'is_read': False,
+        #                                                 'is_important': False,
+        #                                                 'is_edited': False,
+        #                                           "height": int(max(len(i * "привет") / 40, 1) * 50),
+        #                                         'send_from_me': i % 2})
 
     def get_validate_message(self, recipient_id):
         messages = get_time_send_sorted_message(current_user.CURRENT_USER_ID, recipient_id)
@@ -46,7 +39,13 @@ class DialogScreen(BaseScreen):
 
     def reload_messages(self):
         messages = self.get_validate_message(2)
-        self.messages.clear_widgets()
+        self.scrollable_messages.data = []
         for message in messages:
             message: Message
-            self.messages.add_widget(MessageWidget(message))
+            data = message.dict()
+            data["message_text"] = data.pop("text")
+            if data["id"] == current_user.CURRENT_USER_ID:
+                data["send_from_me"] = 1
+            else:
+                data["send_from_me"] = 0
+            self.scrollable_messages.data.append(data)
