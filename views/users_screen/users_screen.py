97c71320200cee6d2bcb1c818bc26d20fd071279
Liza <e.avdeeva00@mail.ru>
2022-01-19 22:18:01 +0300

    users_screen done

--- views/users_screen/users_screen.py
+++ views/users_screen/users_screen.py
@@ -1,5 +1,6 @@
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
+from kivymd.uix.list import OneLineAvatarListItem, ImageLeftWidget
 import requests
 
 from views.base import BaseScreen
@@ -12,7 +13,10 @@ class UsersScreen(BaseScreen):
     SCREEN_NAME = meta.SCREENS.USERS_SCREEN
     users_list = ObjectProperty()
 
-    def __init__(self, **kwargs):
-        super(UsersScreen, self).__init__(**kwargs)
-        req = requests.get("http://127.0.0.1:8000/users").json()
-        print(req)
+    def on_enter(self, *args):
+        self.users_list.clear_widgets()
+        all_users = requests.get("http://127.0.0.1:8000/users")
+        for user in all_users.json():
+            item = OneLineAvatarListItem(text=f"{user['name']} {user['surname']}")
+            item.add_widget(ImageLeftWidget(source=user['photo']))
+            self.users_list.add_widget(item)
