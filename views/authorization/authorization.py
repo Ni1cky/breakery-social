e5a72cb56250e7ffbc5ceadf5377fd3d7b172b12
vadlord <vadim.t0lstykh@yandex.ru>
2022-01-21 23:49:30 +0300

    remember_me done

--- views/authorization/authorization.py
+++ views/authorization/authorization.py
@@ -1,5 +1,7 @@
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
+from kivymd.uix.selectioncontrol import MDCheckbox
+
 from controllers.authorization import get_token
 from views.base import BaseScreen
 from views import meta
@@ -11,6 +13,7 @@ class LoginScreen(BaseScreen):
     SCREEN_NAME = meta.SCREENS.LOGIN_SCREEN
     login = ObjectProperty()
     passw = ObjectProperty()
+    remember_me: MDCheckbox = ObjectProperty()
 
     def on_enter(self, *args):
 
@@ -21,8 +24,10 @@ class LoginScreen(BaseScreen):
     def go_to_main_screen(self):
         access_token = get_token(self.login.text, self.passw.text)
         if access_token != None:
-            f = open('saved\\access_token', 'w')
-            f.write(access_token)
+            if self.remember_me.active:
+                f = open('saved\\access_token', 'w')
+                f.write(access_token)
+                f.close()
             self.manager.current = meta.SCREENS.PROFILE_SCREEN
         else:
             pass
