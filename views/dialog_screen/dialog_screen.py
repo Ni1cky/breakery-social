984d392f815cb9ece3c5575efb5762b47791eafe
Chernenkiy <nikmbogdanov@gmail.com>
2022-01-23 20:10:12 +0300

    Nothing useful

--- views/dialog_screen/dialog_screen.py
+++ views/dialog_screen/dialog_screen.py
@@ -5,6 +5,7 @@ from kivy.properties import ObjectProperty
 from kivy.uix.recycleview import RecycleView
 from kivymd.uix.textfield import MDTextField
 
+from controllers.authorization import get_my_profile
 from controllers.dialogs import get_dialog
 from controllers.user import get_current_user
 from store.models.models import Message, MessageCreate
@@ -23,29 +24,21 @@ class DialogScreen(BaseScreen):
     def __init__(self, id=1, **kwargs):
         super().__init__(**kwargs)
         self.scrollable_messages.data = []
-        self.data = get_dialog(id)
-        # for i in range(1, 40):
-        #     print(len(i * "привет") // 20 * 100)
-        #     self.scrollable_messages.data.append({'message_text': i * "привет)",
-        #                                                 'time_send': datetime.datetime(2019, 6, 1, 12, 22),
-        #                                                 'is_read': False,
-        #                                                 'is_important': False,
-        #                                                 'is_edited': False,
-        #                                           "height": int(max(len(i * "привет") / 40, 1) * 50),
-        #                                         'send_from_me': i % 2})
+        # self.data = get_dialog(id)
 
     def get_validate_message(self, recipient_id):
         current_user = get_current_user()
         messages = get_time_send_sorted_message(current_user.id, recipient_id)
         for message in messages:
-            if message.sender_id == requests.get("http://127.0.0.1:8000/users/me").json()["id"]:
+            if message.sender_id == get_current_user().id:
                 message.send_from_me = True
             else:
                 message.send_from_me = False
         return messages
 
     def reload_messages(self):
-        messages = self.get_validate_message(2)
+        # messages = self.get_validate_message(2)
+        messages = []
         self.scrollable_messages.data = []
         for message in messages:
             message: Message
