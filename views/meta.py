544c94d606bbc9696c08ac02772992ed4f2ce8a2
vadlord <vadim.t0lstykh@yandex.ru>
2022-01-27 16:09:32 +0300

    add people_scree on click on people

--- views/meta.py
+++ views/meta.py
@@ -8,12 +8,22 @@ class SCREENS:
     MAINMENU_SCREEN = 'MainMenuScreen'
     MESSAGE_SCREEN = 'MessageScreen'
     PROFILE_SCREEN = 'ProfileScreen'
+    PEOPLE_SCREEN = 'PeopleScreen'
     NEWS_SCREEN = 'NewsScreen'
     MY_NEWS_SCREEN = 'MyNewsScreen'
     DIALOG_SCREEN = "DialogScreen"
     USERS_SCREEN = 'UsersScreen'
     SUBSCRIBERS_SCREEN = 'SubscribersScreen'
     SUBSCRIPTIONS_SCREEN = 'SubscriptionsScreen'
+
+class CLICK_USER:
+    USER_ID = 0
+    LOGIN = ''
+    NAME = ''
+    SURNAME = ''
+    PHOTO = ''
+    ADDITIONAL_DATA = ''
+    POSTS = []
 
 class HOST:
     URL = 'http://127.0.0.1:8000'
