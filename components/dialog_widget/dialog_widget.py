19c446a68a37e6c1a473492bd3d3e32520bc0b75
Chernenkiy <nikmbogdanov@gmail.com>
2022-01-28 21:39:14 +0300

    Dialog widget now load person's name and surname into it's text in __init__

--- components/dialog_widget/dialog_widget.py
+++ components/dialog_widget/dialog_widget.py
@@ -1,13 +1,21 @@
 from kivy.lang import Builder
 from kivymd.uix.list import TwoLineAvatarIconListItem
-from store.models.models import Message
+from controllers.authorization import get_my_profile
+from controllers.dialog import get_dialog_by_id
+from controllers.user import get_user
+from store.models.models import User
 
 Builder.load_file('components/dialog_widget/dialog_widget.kv')
 
 
 class DialogWidget(TwoLineAvatarIconListItem):
-    def __init__(self, message: Message, name="user", last_message="кинуть физику)", **kwargs):
-        super().__init__(text=name, secondary_text=last_message, **kwargs)
-        self.data = message
-        self.name = name
-        self.last_message = last_message
+    def __init__(self, dialog_id: int, **kwargs):
+        super().__init__(**kwargs)
+        self.dialog = get_dialog_by_id(dialog_id)
+        self.init_text()
+
+    def init_text(self):
+        current_user = User(**get_my_profile())
+        second_person_id = self.dialog.user1_id if current_user.id != self.dialog.user1_id else self.dialog.user2_id
+        second_person = User(**get_user(second_person_id))
+        self.text = f"{second_person.name} {second_person.surname}"
