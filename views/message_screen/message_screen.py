02acccbf6a830cb116c2437c90f068b1a37bcfd1
Chernenkiy <nikmbogdanov@gmail.com>
2022-01-30 22:04:17 +0300

    Useless code deleted

--- views/message_screen/message_screen.py
+++ views/message_screen/message_screen.py
@@ -1,7 +1,6 @@
 from kivy.properties import ObjectProperty
 from kivy.uix.recycleboxlayout import RecycleBoxLayout
 from kivy.uix.recycleview import RecycleView
-from components.dialog_widget.dialog_widget import DialogWidget
 from views.base import BaseScreen
 from views.meta import SCREENS
 from kivy.lang import Builder
@@ -13,6 +12,3 @@ class MessageScreen(BaseScreen):
     SCREEN_NAME = SCREENS.MESSAGE_SCREEN
     dialogs_list: RecycleBoxLayout = ObjectProperty()
     dialogs_recycle_view: RecycleView = ObjectProperty()
-
-    def add_dialog_widget(self, dialog_widget: DialogWidget):
-        self.dialogs_recycle_view.data.append({"dialog_id": dialog_widget.dialog.id})
