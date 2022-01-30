4a36751717070c3fdd3e731148768c9692ece316
Chernenkiy <nikmbogdanov@gmail.com>
2022-01-30 22:00:13 +0300

    Messages now can be sent

--- views/dialog_screen/dialog_screen.py
+++ views/dialog_screen/dialog_screen.py
@@ -1,13 +1,10 @@
 import datetime
-import requests
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.recycleview import RecycleView
 from kivymd.uix.textfield import MDTextField
-from controllers.authorization import get_my_profile
-from store.models.models import Message, MessageCreate
+from store.models.models import MessageCreate
 from kivymd.uix.list import TwoLineAvatarIconListItem
-
 from controllers.authorization import get_my_profile
 from controllers.dialog import get_dialog_by_id
 from controllers.user import get_user
@@ -40,22 +37,24 @@ class DialogScreen(BaseScreen):
         #                                         'send_from_me': i % 2})
 
     def get_validate_message(self, recipient_id):
-        messages = get_time_send_sorted_message(get_my_profile()["id"], recipient_id)
+        current_user = get_my_profile()
+        messages = get_time_send_sorted_message(current_user["id"], recipient_id)
         for message in messages:
-            if message.id == recipient_id:
+            if message.sender_id == current_user["id"]:
                 message.send_from_me = True
             else:
                 message.send_from_me = False
         return messages
 
     def reload_messages(self):
+        current_user = get_my_profile()
         messages = self.get_validate_message(self.dialog_data["another_user_id"])
         self.scrollable_messages.data = []
         for message in messages:
             message: Message
             data = message.dict()
             data["message_text"] = data.pop("text")
-            if data["id"] == get_my_profile()["id"]:
+            if data["sender_id"] == current_user["id"]:
                 data["send_from_me"] = 1
             else:
                 data["send_from_me"] = 0
@@ -63,13 +62,19 @@ class DialogScreen(BaseScreen):
 
     def send_message(self):
         current_user = get_my_profile()
-        message = MessageCreate(text=self.message_text.text, time_send=datetime.datetime.now(), sender_id=current_user["id"], dialog_id=self.dialog_data["dialog_id"])
+        message = MessageCreate(
+            text=self.message_text.text,
+            time_send=str(datetime.datetime.now()),
+            sender_id=current_user["id"],
+            dialog_id=self.dialog_data["dialog_id"]
+        )
         create_message(message)
 
     def load_dialog(self, dialog_id: int):
+        current_user = get_my_profile()
         self.dialog_data = None
         dialog = get_dialog_by_id(dialog_id)
-        if dialog.user1_id == get_my_profile()["id"]:
+        if dialog.user1_id == current_user["id"]:
             self.dialog_data = {"current_user_id": dialog.user1_id, "another_user_id": dialog.user2_id}
             another_user = get_user(dialog.user2_id)
             self.header.text = f"{another_user['name']} {another_user['surname']}"
