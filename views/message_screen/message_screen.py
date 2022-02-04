b8882d6d73db8cf8c6e6ad9b30526ef535ad1723
Chernenkiy <nikmbogdanov@gmail.com>
2022-02-04 20:49:53 +0300

    Just nothing

--- views/message_screen/message_screen.py
+++ views/message_screen/message_screen.py
@@ -1,6 +1,8 @@
 from kivy.properties import ObjectProperty
 from kivy.uix.recycleboxlayout import RecycleBoxLayout
 from kivy.uix.recycleview import RecycleView
+from controllers.authorization import get_my_profile
+from controllers.dialog import get_users_dialogs
 from views.base import BaseScreen
 from views.meta import SCREENS
 from kivy.lang import Builder
@@ -12,3 +14,6 @@ class MessageScreen(BaseScreen):
     SCREEN_NAME = SCREENS.MESSAGE_SCREEN
     dialogs_list: RecycleBoxLayout = ObjectProperty()
     dialogs_recycle_view: RecycleView = ObjectProperty()
+
+    def on_enter(self, *args):
+        get_users_dialogs(get_my_profile()["id"])
